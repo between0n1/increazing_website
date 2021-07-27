3804331829abd9c253d5a85a2562d602c1d243e5
between0n1 <sunhyeok.jung22@myhunter.cuny.edu>
2021-07-27 16:11:56 -0400

    commit

--- app.py
+++ app.py
@@ -45,5 +45,4 @@ def home():
     data = SocialMedias
     return render_template("home.html", data = data)
 if __name__ == '__main__':
-    # sort by tweet_volume
     app.run(host = '0.0.0.0', port = 5000)
