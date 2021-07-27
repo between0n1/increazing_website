aaf45070399610579cc42346160ba1c962c708e5
between0n1 <sunhyeok.jung22@myhunter.cuny.edu>
2021-07-26 20:40:31 -0400

    website shows top 15 trending hashtags from Twitter

--- app.py
+++ app.py
@@ -1,11 +1,46 @@
-from flask import Flask
+import tweepy
+import os
+from flask import Flask, render_template
 from classes import *
+import APIkeys
+
+#Twitter
+
+#API Keys
+consumer_key = APIkeys.APIKEY
+consumer_secret = APIkeys.APISecretKey
+access_token = APIkeys.AccessToken
+access_token_secret = APIkeys.AccessTokenSecret
+
+#Authorization and Authentificatgion
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+#get trending twits
+t = api.trends_place(1)[0]['trends']
+twt_trends = []
+for i in t:
+    name = i['name']
+    url = i['url']
+    twt_vol = i['tweet_volume']
+    twt = Twt(title=name, link = url, volume=twt_vol)
+    if twt_vol != None:
+        twt_trends.append(twt)
+twt_trends.sort(reverse=True)
+twt_trends = twt_trends[:15]
+#Entire database
+SocialMedias = Platforms()
+Twitter = Source(name="Twitter", posts = twt_trends)
+SocialMedias.add(Twitter)
 
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder= "static")
 @app.route('/')
 def home():
-    return "Welcome to my Flask WebSite"
+    data = SocialMedias
+    return render_template("home.html", data = data)
 if __name__ == '__main__':
+    # sort by tweet_volume
     app.run()
--- classes.py
+++ classes.py
@@ -1,8 +1,31 @@
+class Platforms:
+    def __init__(self):
+        self.sources = {}
+        self.count = 0
+    def add(self, source):
+        self.sources[source.name] = source
+        self.count += 1
+    def display(self):
+        for i in self.sources:
+            print(i)
+    def get(self, sourceName):
+        if self.sources.get(sourceName) != None:
+            return self.sources.get(sourceName)
+
+
+
 class Source: # instagram, reddit, twitter
-    def __init__(self, posts): # posts = array of post object
+    def __init__(self, name, posts): # posts = array of post object
+        self.name = name
         self.posts = posts
+    def __repr__(self):
+        return self.name
+    def display(self):
+        for elem in self.posts:
+            print(elem.title, elem.volume)
+
 class Post: # a post from sources
-    def __init__(self, title, text, captions, images, link):
+    def __init__(self, title = "", text = "", captions = None, images = None, link = None):
         self.title = title
         self.text = text
         self.images = images
@@ -10,4 +33,15 @@ class Post: # a post from sources
         self.link = link
     def __repr__(self):
         return self.title + ", " + self.text
+
+class Twt(Post):
+    def __init__(self, title = "", text= "", link = None, volume = None):
+        super().__init__(title= title, text= text, link= link)
+        self.volume = volume
+    def __repr__(self):
+        return f"title: {self.title} volume: {str(self.volume)} link: {self.link}"
+    def __lt__(self, other):
+        return self.volume < other.volume
+    def __eq__(self, other):
+        return self.volume == other.volume
 
